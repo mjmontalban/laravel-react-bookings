cee7753284bbf1aa15fe5af7f84e6f024ab59502
mjmontalban <mj3@viptutorsco@gmail.com>
2025-03-23 21:49:34 +0800

    bookings

---
+++ resources/js/components/card.tsx
@@ -0,0 +1,27 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
+
+
+export default function CardContainer() {
+  return (
+    <Card className="w-full h-full">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        
+      </CardContent>
+      <CardFooter className="flex justify-between">
+   
+      </CardFooter>
+    </Card>
+  )
+}
--- resources/js/pages/bookings.tsx
+++ resources/js/pages/bookings.tsx
@@ -2,8 +2,8 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-
-
+import DataTable from '@/components/table';
+import CardContainer from '@/components/card';
 const breadcrumbs : BreadcrumbItem[] = [
     {
         title: 'Bookings',
@@ -19,7 +19,9 @@ export default function Bookings() {
                     <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                             <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                                <div className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20"> </div>
+                                <div className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20"> 
+                                    <CardContainer />
+                                </div>
                             </div>
                             <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
                                 <div className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20"> </div>
@@ -30,7 +32,7 @@ export default function Bookings() {
                         </div>
                         <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
                             <div className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20">
-                                
+                                <DataTable />
                             </div>
                         </div>
                     </div>
