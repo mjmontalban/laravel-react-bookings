7d38b1988f3e3c968f59b30b29799a932f078533
mjmontalban <mj3@viptutorsco@gmail.com>
2025-03-23 19:32:07 +0800

    fix menu pages

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,19 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::get('bookings', function () {
+        return Inertia::render('bookings');
+    })->name('bookings');
+
+    Route::get('users', function () {
+        return Inertia::render('users');
+    })->name('users');
+
+    Route::get('payments', function () {
+        return Inertia::render('payments');
+    })->name('payments');
+
 });
 
 require __DIR__.'/settings.php';
