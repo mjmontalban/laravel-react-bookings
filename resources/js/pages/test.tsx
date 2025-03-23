1d10f6a82d3f01d4e12bbb879495c647d05b5783
mjviptutors <mj3@viptutorsco@gmail.com>
2025-03-23 18:44:37 +0800

    first commit

---
+++ resources/js/pages/test.tsx
@@ -0,0 +1,13 @@
+import { type SharedData } from '@/types';
+import { Head, Link, usePage } from '@inertiajs/react';
+
+
+
+export default function Test() {
+    return (
+        <>
+            <Head title="Test Page" />
+            <h1>This is the Test Page</h1>
+        </>
+    );
+}
