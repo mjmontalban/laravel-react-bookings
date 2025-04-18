540d0b9394c8fce5e0c2061fc8e5ff4f399374c2
mjmontalban <mj3@viptutorsco@gmail.com>
2025-04-18 19:14:24 +0800

    bookings

--- resources/js/components/ui/confirmation/dialog.tsx
+++ resources/js/components/ui/confirmation/dialog.tsx
@@ -10,7 +10,8 @@ import {
     AlertDialogTrigger,
   } from "@/components/ui/alert-dialog"
   import { Button } from "@/components/ui/button"
-  
+  import { Loader2 } from "lucide-react";
+  import { useState } from 'react';
   type AlertConfirmationProps = {
     id: number;
     // onConfirm?: (id: number) => void;
@@ -18,9 +19,14 @@ import {
 
   export default function AlertConfirmation(props: AlertConfirmationProps) {
 
+    const [ loading, setLoading ] = useState(false);
+    const [ disable, setDisable ] = useState(false);
+
     const handleDelete = (id: number) => {
-        alert(id);
+        setLoading(true);
+        setDisable(true);
     }
+
     return (
       <AlertDialog>
         <AlertDialogTrigger asChild>
@@ -28,15 +34,18 @@ import {
         </AlertDialogTrigger>
         <AlertDialogContent>
           <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogTitle>Are you absolutely sure to delete this booking?</AlertDialogTitle>
             <AlertDialogDescription>
               This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              booking and remove your data from our servers.
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction onClick={ () => handleDelete(props.id)}>Continue</AlertDialogAction>
+            <Button disabled={disable} variant={'outline'}>Cancel</Button>
+            <Button variant={'outline'} onClick={ () => handleDelete(props.id)}>
+                {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                {loading ? "Please wait" : "Continue"}
+            </Button>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
