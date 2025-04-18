2716b9386513ffb4d7fd6751fbc8266ac78dd0c5
mjmontalban <mj3@viptutorsco@gmail.com>
2025-04-18 18:26:49 +0800

    bookings

--- resources/js/components/table.tsx
+++ resources/js/components/table.tsx
@@ -10,8 +10,24 @@ import {
   } from "@/components/ui/table"
 
   import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+
+  import {
     Button
   } from "@/components/ui/button"
+
+  // import {
+  //   AlertConfirmation
+  // } from "@/components/ui/confirmation/dialog"
 
   import { Trash2 } from 'lucide-react';
 
