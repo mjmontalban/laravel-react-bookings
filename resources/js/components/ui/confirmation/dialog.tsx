b5ed4c1208f08db0b381e3b3d79b41860e840a7f
mjmontalban <mj3@viptutorsco@gmail.com>
2025-04-18 19:41:26 +0800

    bookings

--- resources/js/components/ui/confirmation/dialog.tsx
+++ resources/js/components/ui/confirmation/dialog.tsx
@@ -12,14 +12,11 @@ import {
   import { Button } from "@/components/ui/button"
   import { Loader2 } from "lucide-react";
   import { useState } from 'react';
-  
+
   type AlertConfirmationProps = {
     id: number;
     onDelete?: (id: number) => void;
   };
-
-  import api from "@/axios/config";
-
   export default function AlertConfirmation(props: AlertConfirmationProps) {
 
     const [ loading, setLoading ] = useState(false);
