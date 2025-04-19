4c4169c70b06858fcb9b142672c2cc22c5fce511
mjmontalban <mj3@viptutorsco@gmail.com>
2025-04-19 22:43:40 +0800

    bookings

--- resources/js/pages/bookings.tsx
+++ resources/js/pages/bookings.tsx
@@ -3,7 +3,6 @@ import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 import DataTable from '@/components/table';
-// import CardContainer from '@/components/card';
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -25,19 +24,7 @@ export default function Bookings() {
         <AppLayout breadcrumbs={breadcrumbs}>
                     <Head title="Bookings" />
                     <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                        {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                            <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                                <div className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20"> 
-                                    <CardContainer />
-                                </div>
-                            </div>
-                            <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                                <div className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20"> </div>
-                            </div>
-                            <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                                <div className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20"> </div>
-                            </div>
-                        </div> */}
+                
                         <div className="flex w-full max-w-7xl items-center gap-2 px-4xx">
                             <Input type="text" placeholder="Input any keyword" className="flex-1" />
                             <PlatformSelect />
