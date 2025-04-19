035d6d275beda964cd45ba61f24e3eb9db54847a
mjmontalban <mj3@viptutorsco@gmail.com>
2025-04-19 23:14:16 +0800

    bookings

--- resources/js/components/table.tsx
+++ resources/js/components/table.tsx
@@ -2,8 +2,6 @@ import {
     Table,
     TableBody,
     TableCaption,
-    TableCell,
-    TableFooter,
     TableHead,
     TableHeader,
     TableRow,
@@ -44,13 +42,6 @@ import {
           </TableRow>
         ))}
         </TableBody>
-
-        {/* <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
-            <TableCell className="text-right">$2,500.00</TableCell>
-          </TableRow>
-        </TableFooter> */}
       </Table>
     )
   }
