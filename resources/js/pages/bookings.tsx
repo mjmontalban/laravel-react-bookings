15302f3f1dead348b58465297964dd652e207fd8
mjmontalban <mj3@viptutorsco@gmail.com>
2025-04-19 22:09:27 +0800

    bookings

--- resources/js/pages/bookings.tsx
+++ resources/js/pages/bookings.tsx
@@ -3,7 +3,11 @@ import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 import DataTable from '@/components/table';
-import CardContainer from '@/components/card';
+// import CardContainer from '@/components/card';
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
 const breadcrumbs : BreadcrumbItem[] = [
     {
         title: 'Bookings',
@@ -30,6 +34,12 @@ export default function Bookings() {
                                 <div className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20"> </div>
                             </div>
                         </div> */}
+                        <div className="flex w-full max-w-7xl items-center gap-2 px-4xx">
+                            <Input type="text" placeholder="Input any keyword" className="flex-1" />
+                            <Input type="text" placeholder="Input any keyword" className="flex-1"/>
+                            <Input type="text" placeholder="Input any keyword" className="flex-1"/>
+                            <Button type="button">Search</Button>
+                        </div>
                         <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
                             <div className="absolute inset-0 size-full overflow-auto p-4 stroke-neutral-900/20 dark:stroke-neutral-100/20">
                                 <DataTable />
