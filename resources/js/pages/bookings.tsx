699b4aa8efb4bcf8db0df4d529135495433f6924
mjmontalban <mj3@viptutorsco@gmail.com>
2025-04-19 22:45:49 +0800

    bookings

--- resources/js/pages/bookings.tsx
+++ resources/js/pages/bookings.tsx
@@ -22,21 +22,21 @@ const breadcrumbs : BreadcrumbItem[] = [
 export default function Bookings() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-                    <Head title="Bookings" />
-                    <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <Head title="Bookings" />
+                <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 
-                        <div className="flex w-full max-w-7xl items-center gap-2 px-4xx">
-                            <Input type="text" placeholder="Input any keyword" className="flex-1" />
-                            <PlatformSelect />
-                            <PlatformDatePicker />
-                            <Button type="button">Search</Button>
-                        </div>
-                        <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-                            <div className="absolute inset-0 size-full overflow-auto p-4 stroke-neutral-900/20 dark:stroke-neutral-100/20">
-                                <DataTable />
-                            </div>
+                    <div className="flex w-full max-w-7xl items-center gap-2 px-4xx">
+                        <Input type="text" placeholder="Input any keyword" className="flex-1" />
+                        <PlatformSelect />
+                        <PlatformDatePicker />
+                        <Button type="button">Search</Button>
+                    </div>
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
+                        <div className="absolute inset-0 size-full overflow-auto p-4 stroke-neutral-900/20 dark:stroke-neutral-100/20">
+                            <DataTable />
                         </div>
                     </div>
-                </AppLayout>
+                </div>
+        </AppLayout>
     );
 }
